e4d8fbc768c113bda081b789b41af2c14b3d116c
Julie Hwang <julie.hwang@marqvision.com>
2020-10-19 13:23:21 +1100

    no message

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -661,7 +661,7 @@ export default class Graph extends React.Component {
       <div id={`${this.state.id}-${CONST.GRAPH_WRAPPER_ID}`}>
         <svg name={`svg-container-${this.state.id}`} style={svgStyle} onClick={this.onClickGraph}>
           {defs}
-          {!this.state.focusedNodeId && (
+          {!this.state.highlightedNode && (
             <>
               <circle cx="350" cy="350" r="300" stroke="#e4e9f2" fill="transparent" stroke-width="1"></circle>
               <circle cx="350" cy="350" r="140" stroke="#e4e9f2" fill="transparent" stroke-width="1"></circle>
